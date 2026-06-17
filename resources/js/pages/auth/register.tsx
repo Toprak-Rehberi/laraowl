e11623251b4b944b6570eff5204bf3a322835cba
Abdelmjid Saber <abdelmjid999@gmail.com>
2026-06-17 20:55:05 +0300

    use correct store import in register page

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -6,8 +6,8 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Spinner } from '@/components/ui/spinner';
+import { store } from '@/actions/Laravel/Fortify/Http/Controllers/RegisteredUserController';
 import { login } from '@/routes';
-import { store } from '@/routes/register';
 
 export default function Register() {
     return (
